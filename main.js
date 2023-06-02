950c89559868dc58429224e6124a62bac9900657
Tam Nguyen <tamntrung@gmail.com>
2023-06-02 21:42:32 +0000

    Switch views

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const $addEntryButton = document.querySelector('#add-entry-button');
 const $pageContainer = document.querySelector('.page-container');
+const $days = document.querySelector('#days');
 
 const $timeInput = document.querySelector('.time-input');
 const $notesInput = document.querySelector('.notes-input');
@@ -33,3 +34,29 @@ $form.addEventListener('submit', () => {
   $pageContainer.classList.add('hidden');
 
 });
+
+const $daysOfWeek = document.querySelectorAll('.days-of-the-week');
+const $scheduleText = document.querySelector('.scheduled-events-text');
+
+$days.addEventListener('click', function (event) {
+  if (event.target.matches('.days-of-the-week')) {
+    for (let i = 0; i < $daysOfWeek.length; i++) {
+      if ($daysOfWeek[i].getAttribute('data-view') === 'sun-view') {
+        $scheduleText.textContent = 'Scheduled Events for Sunday';
+      } else if ($daysOfWeek[i].getAttribute('data-view') === 'mon-view') {
+        $scheduleText.textContent = 'Scheduled Events for Monday';
+      } else if ($daysOfWeek[i].getAttribute('data-view') === 'tue-view') {
+        $scheduleText.textContent = 'Scheduled Events for Tuesday';
+      } else if ($daysOfWeek[i].getAttribute('data-view') === 'wed-view') {
+        $scheduleText.textContent = 'Scheduled Events for Wednesday';
+      } else if ($daysOfWeek[i].getAttribute('data-view') === 'thu-view') {
+        $scheduleText.textContent = 'Scheduled Events for Thursday';
+      } else if ($daysOfWeek[i].getAttribute('data-view') === 'fri-view') {
+        $scheduleText.textContent = 'Scheduled Events for Friday';
+      } else if ($daysOfWeek[i].getAttribute('data-view') === 'sat-view') {
+        $scheduleText.textContent = 'Scheduled Events for Saturday';
+      } else {
+        break;
+      }
+    }
+);
