c43dc22d0d9cd520757eb7de1653130313fdf38c
Tam Nguyen <tamntrung@gmail.com>
2023-06-02 19:11:00 +0000

    stored to local storage

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
-// const $addEntryButton = document.querySelector('#add-entry-button');
-// const $pageContainer = document.querySelector('.page-container');
+const $addEntryButton = document.querySelector('#add-entry-button');
+const $pageContainer = document.querySelector('.page-container');
 
-// const $timeInput = document.querySelector('.time-input');
-// const $notesInput = document.querySelector('.notes-input');
-// const $dayDropdown = document.querySelector('.day-dropdown');
-// const $form = document.querySelector('.form');
+const $timeInput = document.querySelector('.time-input');
+const $notesInput = document.querySelector('.notes-input');
+const $dayDropdown = document.querySelector('.day-dropdown');
+const $form = document.querySelector('.form');
 
-// $addEntryButton.addEventListener('click', openModal);
+$addEntryButton.addEventListener('click', openModal);
 
-// function openModal(event) {
-//   $pageContainer.classList.remove('hidden');
-// }
+const data = {
+  editing: null,
+  entries: [],
+  nextId: 0
+};
 
-// // $form.addEventListener('submit', () => {
-// //   const entries = {
-// //     day: $dayDropdown,
-// //     time: $timeInput,
-// //     notes: $notesInput,
-// //     id: 'data.nextId'
-// //   };
+function openModal(event) {
+  $pageContainer.classList.remove('hidden');
+}
 
-// //   data.nextId++;
-// // });
+$form.addEventListener('submit', () => {
+  event.preventDefault();
+  const newEntries = {
+    day: $dayDropdown.value,
+    time: $timeInput.value,
+    notes: $notesInput.value,
+    id: data.nextId
+  };
+  data.entries.unshift(newEntries);
+  const jsonData = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', jsonData);
+  data.nextId++;
+  $pageContainer.classList.add('hidden');
+
+});
